858518f0808c33c7ef4e8b7a5ca10e157448a07d
Bryan Choe <bryan.choe88@gmail.com>
2016-10-13 10:59:23 -1000

    works

--- indexing.sql
+++ indexing.sql
@@ -1,12 +1,12 @@
-\c bryanchoe;
+/*\c bryanchoe;
 
-CREATE USER indexed_cars_user;
+CREATE USER indexed_cars_user;*/
 
-DROP DATABASE IF EXISTS indexed_cars;
+/*DROP DATABASE IF EXISTS indexed_cars;*/
 
-\c indexed_cars_user;
+/*CREATE DATABASE indexed_cars WITH OWNER indexed_cars_user;*/
 
-CREATE DATABASE indexed_cars OWNER indexed_cars_user;
+/*\c indexed_cars indexed_cars_user;
 
 \i scripts/car_models.sql
 
@@ -19,10 +19,35 @@ CREATE DATABASE indexed_cars OWNER indexed_cars_user;
 \i scripts/car_model_data.sql
 \i scripts/car_model_data.sql
 \i scripts/car_model_data.sql
-\i scripts/car_model_data.sql
+\i scripts/car_model_data.sql*/
+
+
 
 CREATE INDEX make_titles
   ON car_models (make_title);
 
- CREATE INDEX model_titles
+CREATE INDEX model_titles
   ON car_models (model_title);
+
+SELECT DISTINCT make_title FROM car_models
+WHERE make_code = 'LAM';
+
+SELECT DISTINCT model_title FROM car_models
+WHERE make_code = 'NISSAN' AND model_code = 'GT-R';
+
+SELECT make_code, model_code, model_title FROM car_models
+WHERE make_code = 'LAM';
+
+SELECT * FROM car_models
+WHERE year BETWEEN '2010' AND '2015';
+
+SELECT * FROM car_models
+WHERE year = '2010';
+
+--Timing
+--Lamborghini 61.905ms
+--GT-R 58.962ms
+--Lamborghini all 57.789
+--2010 to 2015 177.794ms
+--2010 85.947ms
+
